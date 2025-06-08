d1e38e7758af216dd62103326361e57b5da8c751
PA1IN <d.ramosguerra018@gmail.com>
2025-06-07 20:50:00 -0400

    authUser

---
+++ authUser/auth-user/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {}
+
+    findByEmail(email: string): Promise<User | null> {
+        return this.usersRepository.findOne({ where: { email }});
+    }
+
+    create(userData: Partial<User>): Promise<User> {
+        const usuarioNuevo = this.usersRepository.create(userData);
+        return this.usersRepository.save(usuarioNuevo);
+    }
+}
